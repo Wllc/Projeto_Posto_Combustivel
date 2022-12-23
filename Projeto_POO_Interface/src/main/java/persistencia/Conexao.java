e5b79892a70f6b74eaf9768ba707a1ec68b28472
Wallace Gabriel <50117208+Wllc@users.noreply.github.com>
2022-12-23 15:59:50 -0300

    Update Conexao.java

--- Projeto_POO_Interface/src/main/java/persistencia/Conexao.java
+++ Projeto_POO_Interface/src/main/java/persistencia/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Conexao {
     public static Connection getConexao() throws SQLException {
         String usuario = "postgres";
-        String senha = "0233";
+        String senha = "****";
         String caminho = "jdbc:postgresql://localhost:5432/postoCombustivel";
         return DriverManager.getConnection(caminho, usuario, senha);
     }
